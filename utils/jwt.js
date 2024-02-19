0b925ff4bb5bca230e75a6bfb80e639098b30506
Alexandr Gavrik <sgavrik03@yandex.ru>
2024-02-19 12:49:45 +0300

    serverComplete

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const jwt = require('jsonwebtoken');
 
-const generateToken = (payload) => jwt.sign(payload, 'dev_secret', {
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
+
+const { JWT_SECRET, NODE_ENV } = process.env;
+const generateToken = (payload) => jwt.sign(payload, NODE_ENV !== 'production' ? 'jwt_secret' : JWT_SECRET, {
   expiresIn: 604800,
 });
 
